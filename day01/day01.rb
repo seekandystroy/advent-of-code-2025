b077a8c011638f00f6438827be704e5c20158a0e
Andy Gonçalves <agoncalves1@gmail.com>
2025-12-01 22:24:58 +0000

    chore: set up boilerplate

---
+++ day01/day01.rb
@@ -0,0 +1,5 @@
+module Day01
+  def self.part1
+    puts 'ok'
+  end
+end
---
+++ main.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative('day01/day01')
+
+case ARGV[0]
+when '0101'
+  Day01.part1
+else
+  puts('Please choose a day and part to run, in the format DDPP.')
+end
