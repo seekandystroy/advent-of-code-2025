ac59473e66e4e097731149c245364813b18fb52f
Andy Gonçalves <agoncalves1@gmail.com>
2025-12-01 23:32:41 +0000

    feat: add day 1, part 1

--- day01/day01.rb
+++ day01/day01.rb
@@ -1,5 +1,25 @@
+# frozen_string_literal: true
+
 module Day01
   def self.part1
-    puts 'ok'
+    ptr = 50
+    zero_count = 0
+
+    File.foreach('day01/test_input.txt') do |line|
+      match = line.match(/([LR])(\d+)/)
+
+      dir = match[1]
+      amount = match[2].to_i
+
+      ptr = if dir == 'L'
+              (ptr - amount - 100) % 100
+            else
+              (ptr + amount + 100) % 100
+            end
+
+      zero_count += 1 if ptr == 0
+    end
+
+    puts zero_count
   end
 end
