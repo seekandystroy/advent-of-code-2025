1ddb2b6a070b114bae84f21ff6c839f80d77a140
Andy Gonçalves <agoncalves1@gmail.com>
2025-12-02 01:02:29 +0000

    feat: add day 1, part 2

--- day01/day01.rb
+++ day01/day01.rb
@@ -22,4 +22,27 @@ module Day01
 
     puts zero_count
   end
+
+  def self.part2
+    ptr = 50
+    zero_count = 0
+
+    File.foreach('day01/test_input.txt') do |line|
+      match = line.match(/([LR])(\d+)/)
+
+      dir = match[1]
+      amount = match[2].to_i
+
+      ptr = if dir == 'L'
+              zero_count += (ptr - 100 - amount) / -100
+              zero_count -= 1 if ptr == 0 # when pointer is 0, the op above will be off by 1
+              (ptr - amount - 100) % 100
+            else
+              zero_count += (ptr + amount) / 100
+              (ptr + amount + 100) % 100
+            end
+    end
+
+    puts zero_count
+  end
 end
--- main.rb
+++ main.rb
@@ -5,6 +5,8 @@ require_relative('day01/day01')
 case ARGV[0]
 when '0101'
   Day01.part1
+when '0102'
+  Day01.part2
 else
   puts('Please choose a day and part to run, in the format DDPP.')
 end
