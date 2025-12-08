96fb0ba3cb83ef85ab92e8a132a0f2a4d1349997
Andy Gonçalves <agoncalves1@gmail.com>
2025-12-08 15:58:35 +0000

    refactor: DRY up day 8

--- day08/day08.rb
+++ day08/day08.rb
@@ -5,13 +5,7 @@ module Day08
     boxes = File.readlines('day08/test_input.txt', chomp: true)
                 .map { |line| line.scan(/\d+,?/).map(&:to_i) }
 
-    distances = []
-    boxes.combination(2) do |combo|
-      distance = euclidean_distance(combo[0], combo[1])
-      distances << [distance, combo[0], combo[1]]
-    end
-    distances.sort_by! { |dc| dc[0] }
-
+    distances = sorted_distances_between_boxes(boxes)
     circuits = connect_closest(distances, 10)
 
     circuits.values.map(&:size).sort.last(3).reduce(&:*)
@@ -21,16 +15,21 @@ module Day08
     boxes = File.readlines('day08/test_input.txt', chomp: true)
                 .map { |line| line.scan(/\d+,?/).map(&:to_i) }
 
+    distances = sorted_distances_between_boxes(boxes)
+    p1, p2 = connect_until_all_in_one(distances, boxes.size)
+
+    p1[0] * p2[0]
+  end
+
+  def self.sorted_distances_between_boxes(boxes)
     distances = []
+
     boxes.combination(2) do |combo|
       distance = euclidean_distance(combo[0], combo[1])
       distances << [distance, combo[0], combo[1]]
     end
+
     distances.sort_by! { |dc| dc[0] }
-
-    p1, p2 = connect_until_all_in_one(distances, boxes.size)
-
-    p1[0] * p2[0]
   end
 
   def self.euclidean_distance(p1, p2)
@@ -54,21 +53,7 @@ module Day08
 
       next if !circuit_p1_id.nil? && circuit_p1_id == circuit_p2_id
 
-      new_circuit = if circuit_p1_id == circuit_p2_id
-                      [p1, p2].to_set
-                    else
-                      circuit_p1 = circuits[circuit_p1_id] || Set.new
-                      circuit_p2 = circuits[circuit_p2_id] || Set.new
-
-                      circuits.delete(circuit_p1_id)
-                      circuits.delete(circuit_p2_id)
-
-                      circuit_p1 | circuit_p2 | [p1, p2]
-                    end
-
-      circuits[next_id] = new_circuit
-      new_circuit.each { |pos| circuits_rindex[pos] = next_id }
-      next_id += 1
+      next_id = update_circuits_and_rindex(circuits, circuits_rindex, next_id, p1, p2)
     end
 
     circuits
@@ -85,31 +70,40 @@ module Day08
       circuit_p1_id = circuits_rindex[p1]
       circuit_p2_id = circuits_rindex[p2]
 
-      next if !circuit_p1_id.nil? && circuit_p1_id == circuit_p2_id
+      next if !circuit_p1_id.nil? && circuits_rindex[p1] == circuit_p2_id
 
-      new_circuit = if circuit_p1_id == circuit_p2_id
-                      [p1, p2].to_set
-                    else
-                      circuit_p1 = circuits[circuit_p1_id] || Set.new
-                      circuit_p2 = circuits[circuit_p2_id] || Set.new
+      next_id = update_circuits_and_rindex(circuits, circuits_rindex, next_id, p1, p2)
 
-                      circuits.delete(circuit_p1_id)
-                      circuits.delete(circuit_p2_id)
-
-                      circuit_p1 | circuit_p2 | [p1, p2]
-                    end
-
-      circuits[next_id] = new_circuit
-      new_circuit.each { |pos| circuits_rindex[pos] = next_id }
-      next_id += 1
-
-      return [p1, p2] if circuits.size == 1 && new_circuit.size == target_circuit_size
+      return [p1, p2] if circuits.size == 1 && circuits.values[0].size == target_circuit_size
     end
 
     []
   end
 
+  def self.update_circuits_and_rindex(circuits, circuits_rindex, next_id, p1, p2)
+    circuit_p1_id = circuits_rindex[p1]
+    circuit_p2_id = circuits_rindex[p2]
+
+    new_circuit = if circuit_p1_id == circuit_p2_id
+                    [p1, p2].to_set
+                  else
+                    circuit_p1 = circuits[circuit_p1_id] || Set.new
+                    circuit_p2 = circuits[circuit_p2_id] || Set.new
+
+                    circuits.delete(circuit_p1_id)
+                    circuits.delete(circuit_p2_id)
+
+                    circuit_p1 | circuit_p2 | [p1, p2]
+                  end
+
+    circuits[next_id] = new_circuit
+    new_circuit.each { |pos| circuits_rindex[pos] = next_id }
+    next_id += 1
+  end
+
+  private_class_method :sorted_distances_between_boxes
   private_class_method :euclidean_distance
   private_class_method :connect_closest
   private_class_method :connect_until_all_in_one
+  private_class_method :update_circuits_and_rindex
 end
